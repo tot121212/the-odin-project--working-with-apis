7f56c8fd6147cb741f1e02160db95a6507f43875
tot121212 <totmusicbusiness@gmail.com>
2025-02-18 21:52:53 -0600

    fix for url

--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
 import "./template.css";
 import {apiKey} from './apiKey.js';
 
-const fetchImgURL = async (searchTerm)=>{
-    const response = fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${searchTerm}`, {mode: "cors"})
-    .then((response)=>{
-        return response.json();
-    })
-    .then((response)=>{
-        console.log("JSON:", response);
-        switch (response.meta.status) {
+const fetchImgURL = async (searchTerm) => {
+    console.log("Search Term:", searchTerm);
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${searchTerm}`, { mode: "cors" });
+        const json = await response.json();
+        
+        console.log("JSON:", json);
+        
+        switch (json.meta.status) {
             case 200:
+                if (!json.data || !json.data.images || !json.data.images.original) {
+                    throw new Error("No image found for search term.");
+                }
+                document.querySelector("img").hidden = false;
+                const url = json.data.images.original.url;
+                console.log("Image URL:", url);
+                return url;
             case 429:
-                throw new Error(response.meta.msg);
+                throw new Error("Too many requests.");
+            
             default:
-                break;
+                throw new Error(json.meta.msg || "Error");
         }
-        img.hidden = false;
-        return response.data.images.original.url;
-    })
-    .catch((error)=>{
-        const img = document.querySelector("img");
-        img.hidden = true;
-        console.log(error);
-    });
-    console.log("Response:", await response);
-    return response;
-}
+
+        return url;
+    } catch (error) {
+        console.error("Error:", error.message);
+        document.querySelector("img").hidden = true;
+        return null;
+    }
+};
 
 
 document.addEventListener("DOMContentLoaded", async ()=>{
     const img = document.querySelector("img");
-    img.src = await fetchImgURL("");
+    let url = await fetchImgURL("");
+    img.src = url;
     const fetchBtn = document.querySelector("button");
     fetchBtn.addEventListener("click", async ()=>{
         let searchTerm = document.querySelector("input").value;
-        const url = await fetchImgURL(searchTerm);
-        if (!url) {
-            return;
-        }
+        url = await fetchImgURL(searchTerm);
+        console.log("Image URL:", url);
         img.src = url;
     });
 });
